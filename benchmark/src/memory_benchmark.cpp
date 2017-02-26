147d4623ce9ca5b9274567e291ae66724f99ae3b
martinradev <martin.b.radev@gmail.com>
2017-02-26 13:39:29 -0800

    Add gpu saturation benchmark

--- benchmark/src/memory_benchmark.cpp
+++ benchmark/src/memory_benchmark.cpp
@@ -49,3 +49,46 @@ static void GenerateFullBenchmark(benchmark::internal::Benchmark* b) {
 }
 
 BENCHMARK(BM_CopyMemory)->Apply(GenerateFullBenchmark)->UseManualTime();
+
+static const unsigned int SaturationBlockSize = 512u;
+static const GLSL_ALGO_READ_WRITE_TYPE SaturationRWSize = GARWTuint4;
+static const unsigned int SaturationElementStep = 2u;
+
+static void BM_SaturationBenchmark(benchmark::State &state)
+{
+	glsl_algo_gl_context glContext;
+	init_window_and_gl_context(&glContext);
+
+	const unsigned int ElementsPerBlock = SaturationBlockSize * SaturationRWSize * SaturationElementStep;;
+	const unsigned n = ElementsPerBlock * state.range(0);
+	std::vector<unsigned> vec = generateIntegralRandomVector(n, 0u, 3u);
+	GLuint inputBuffer = create_ssbo(n, vec.data());
+	GLuint outputBuffer = create_ssbo(n);
+
+	while (state.KeepRunning())
+	{
+		glsl_algo_configuration conf = { SaturationRWSize, SaturationBlockSize, 32 };
+		glsl_algo_context ctx = glsl_algo_init(&glContext, conf);
+
+		BENCHMARK_GPU(glsl_copy_memory, glsl_copy_memory(&glContext, &ctx, inputBuffer, outputBuffer, n, SaturationElementStep));
+	}
+
+	state.SetBytesProcessed(state.iterations() * size_t(n) * sizeof(unsigned) * size_t(2));
+
+	destroy_window_and_gl_context();
+}
+
+static void GenerateSaturationBenchmark(benchmark::internal::Benchmark* b) {
+	
+	const unsigned int ElementsStep = SaturationBlockSize * SaturationRWSize * SaturationElementStep;
+	const unsigned int NumBlocksMin = 64;
+	const unsigned int NumBlocksMax = 12 * 1024;
+	const unsigned int Step = 512;
+
+	for (unsigned int t = NumBlocksMin; t <= NumBlocksMax; t += Step)
+	{
+		b->Args({ static_cast<int>(t) });
+	}
+}
+
+BENCHMARK(BM_SaturationBenchmark)->Apply(GenerateSaturationBenchmark)->UseManualTime();
